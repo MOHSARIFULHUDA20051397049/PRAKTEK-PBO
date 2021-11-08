45d9263f54dafd2fbd1a175dca71ed3c7f23fb1e
Moh. Sariful Huda <72676993+MOHSARIFULHUDA20051397049@users.noreply.github.com>
2021-11-09 05:37:40 +0700

    Add files via upload

---
+++ Prak 4 no1.php
@@ -0,0 +1,50 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="No 1.css">
+    <title>Praktikum 4</title>
+</head>
+<body>
+<div class="container">
+<h2>Soal 1</h2>
+<!-- Program PHP -->
+<?php
+
+class Orang{
+    protected $nama,$umur;
+
+    public function __construct($nama,$umur){
+    $this->nama = $nama;
+    $this->umur = $umur;
+    }
+}
+
+class Dosen extends Orang{
+    private $nip;
+    
+    public function __construct($nama, $umur, $nip){
+    $this->nip = $nip;
+    parent::__construct($nama, $umur);    
+    }
+
+    public function hasil(){
+        return $this->nama . ' berumur ' . $this->umur. ' dengan NIP ' . $this->nip;
+    }
+}
+
+
+$dosen1 = new Dosen('Yaya', 38, 20051390099); 
+$dosen2 = new Dosen('Yoyo', 45, 20051390055);
+$dosen3 = new Dosen('Yosua, 65, 20051390066);
+
+echo $dosen1->hasil();
+echo '<br> <br>';
+echo $dosen2->hasil();
+echo '<br> <br>';
+echo $dosen3->hasil();
+echo '<br> <br>';
+
+?>
+<!-- Akhir Program PHP -->
+</div>
+</body>
+</html>
